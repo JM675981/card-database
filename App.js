460eb6d8c837c8c2748048e035a7239b77e6c319
Joshua McCue <jm675981@southeast.edu>
2023-05-01 21:14:24 -0500

    Project 2 Submit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Pressable, Alert, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable, Alert, Image, ScrollView, Linking } from 'react-native';
 import React, { useState } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ImagePicker from 'expo-image-picker';
+import * as SQLite from "expo-sqlite";
 
 SplashScreen.preventAutoHideAsync();
 setTimeout(SplashScreen.hideAsync, 2000);
@@ -55,26 +56,47 @@ function CardData({ }) {
       setWeb(web);
       const selectedImage = await AsyncStorage.getItem(imageKey);
       setSelectedImage(selectedImage);
+
+      if (selectedImage == 'default') {
+        setSelectedImage(require('./assets/cardIcon.jpg'));
+      } else {
+        setSelectedImage({ uri: selectedImage });
+      }
     } catch (error) {
       Alert.alert('Error', 'One or more items failed to load');
     }
   }
 
+  sendLink = () => {
+    try {
+      Linking.openURL(web);
+    } catch (error) {
+      Alert.alert('Error', 'Invalid link used');
+    }
+  }
+
+  //Source found for Linking: https://stackoverflow.com/questions/30540252/how-does-one-display-a-hyperlink-in-react-native-app
   return (
-    <View style={styles.container}>
-      <Text>{title}</Text>
-      <Text>Players: {min} to {max}{scale == 'false' ? '' : '+'}</Text>
-      <Text>Deck Size: {deck} Cards</Text>
-      <Text>Hand Size: {hand} Cards </Text>
-      <Text>Jokers: {joker > 0 ? joker : 'None'}</Text>
-      <Text>Rules: </Text>
-      <TextInput value={rule} multiline></TextInput>
-      <Text style={styles.link}>{web}</Text>
-      <Image source={{ uri: selectedImage }} style={styles.image} />
+    <ScrollView style={styles.container}>
+
+      <Text style={styles.cardTitle}>{title}</Text>
+      <View style={styles.cardBox}>
+        <Image source={selectedImage} style={styles.imageSmall} />
+        <View>
+          <Text style={styles.cardText}>Players: {min}{max != null && min != max ? ' to ' + max : ''}{scale == 'true' ? '+' : ''}</Text>
+          <Text style={styles.cardText}>Deck Size: {deck} Cards</Text>
+          <Text style={styles.cardText}>Hand Size: {hand} Cards</Text>
+          <Text style={styles.cardText}>Jokers: {joker > 0 ? joker : 'None'}</Text>
+        </View>
+      </View>
+      <Text style={styles.cardText}>Rules: </Text>
+      <TextInput value={rule} style={styles.cardText} multiline></TextInput>
+      <Text style={styles.link} onPress={this.sendLink}>{web}</Text>
 
       <Pressable style={styles.button} onPress={this.onLoad}>
-        <Text style={styles.buttonLabel}>Load</Text></Pressable>
-    </View>
+        <Text style={styles.buttonLabel}>Load</Text>
+      </Pressable>
+    </ScrollView>
   )
 }
 
@@ -89,6 +111,100 @@ function AddCard({ }) {
   const [rule, setRule] = useState('');
   const [web, setWeb] = useState('');
   const [selectedImage, setSelectedImage] = useState(null);
+
+  selected = () => {
+    if (!scale) {
+      setScale(true);
+    } else {
+      setScale(false);
+    }
+  }
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('No image selected. Reverting to default.');
+      setSelectedImage('default');
+    }
+  }
+
+  const validation = () => {
+    let dataValid = true;
+    let errorMessage = '';
+
+    if (title == '') {
+      if (errorMessage != '') {
+        errorMessage += '\n'
+      }
+      errorMessage += 'Title cannot be empty.';
+      dataValid = false;
+    }
+
+    if (isNaN(parseInt(min))) {
+      if (errorMessage != '') {
+        errorMessage += '\n'
+      }
+      errorMessage += 'Min Players must contain a valid integer.';
+      dataValid = false;
+    }
+
+    if (max != '' && isNaN(parseInt(max))) {
+      if (errorMessage != '') {
+        errorMessage += '\n'
+      }
+      errorMessage += 'Max Players must contain a valid integer.';
+      dataValid = false;
+    }
+
+    if (isNaN(parseInt(deck))) {
+      if (errorMessage != '') {
+        errorMessage += '\n'
+      }
+      errorMessage += 'Deck Size must contain a valid integer.';
+      dataValid = false;
+    }
+
+    if (isNaN(parseInt(hand))) {
+      if (errorMessage != '') {
+        errorMessage += '\n'
+      }
+      errorMessage += 'Hand Size must contain a valid integer.';
+      dataValid = false;
+    }
+
+    if (joker != '' && isNaN(parseInt(joker))) {
+      if (errorMessage != '') {
+        errorMessage += '\n'
+      }
+      errorMessage += 'Number of Jokers must contain a valid integer.';
+      dataValid = false;
+    }
+
+    if (rule == '') {
+      if (errorMessage != '') {
+        errorMessage += '\n'
+      }
+      errorMessage += 'Rules cannot be empty.';
+      dataValid = false;
+    }
+
+    if (selectedImage == null) {
+      setSelectedImage('default');
+    }
+
+    if (dataValid) {
+      this.onSave()
+    } else {
+      Alert.alert('Input Error', errorMessage)
+    }
+
+  }
 
   onSave = async () => {
     try {
@@ -108,36 +224,15 @@ function AddCard({ }) {
     }
   }
 
-  selected = () => {
-    if (!scale) {
-      setScale(true);
-    } else {
-      setScale(false);
-    }
-  }
-
-  const pickImageAsync = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);
-    } else {
-      alert('No image selected.');
-    }
-  }
-
   return (
     <ScrollView style={styles.container}>
       <View style={styles.box}>
-        <Text style={styles.label}>Title:</Text>
+        <Text style={styles.label}>*Title:</Text>
         <TextInput style={styles.input} onChangeText={(val) => setTitle(val)}></TextInput>
       </View>
 
       <View style={styles.box}>
-        <Text style={styles.label}>Min Players:</Text>
+        <Text style={styles.label}>*Min Players:</Text>
         <TextInput style={styles.inputNum} onChangeText={(val) => setMin(val)}></TextInput>
         <Text style={styles.label}>Max Players:</Text>
         <TextInput style={styles.inputNum} onChangeText={(val) => setMax(val)}></TextInput>
@@ -150,9 +245,9 @@ function AddCard({ }) {
       </View>
 
       <View style={styles.box}>
-        <Text style={styles.label}>Deck Size:</Text>
+        <Text style={styles.label}>*Deck Size:</Text>
         <TextInput style={styles.inputNum} onChangeText={(val) => setDeck(val)}></TextInput>
-        <Text style={styles.label}>Hand Size:</Text>
+        <Text style={styles.label}>*Hand Size:</Text>
         <TextInput style={styles.inputNum} onChangeText={(val) => setHand(val)}></TextInput>
       </View>
 
@@ -162,27 +257,28 @@ function AddCard({ }) {
       </View>
 
       <View style={styles.box}>
-        <Text style={styles.label}>Rules:</Text>
+        <Text style={styles.label}>*Rules:</Text>
         <TextInput style={styles.inputBox} onChangeText={(val) => setRule(val)} multiline></TextInput>
       </View>
 
       <View style={styles.box}>
-        <Text style={styles.label}>Web:</Text>
-        <TextInput style={styles.input} onChangeText={(val) => setWeb(val)}></TextInput>
+        <Text style={styles.label}>*Web:</Text>
+        <TextInput style={styles.input} onChangeText={(val) => setWeb(val)} placeholder='https://link.com'></TextInput>
       </View>
 
       <View style={styles.box}>
-        <Pressable style={styles.swap} onPress={pickImageAsync}>
-          <Text style={styles.label}>Insert Image</Text>
-        </Pressable>
+        <View>
+          <Pressable style={styles.swap} onPress={pickImageAsync}>
+            <Text style={styles.label}>Insert Image</Text>
+          </Pressable>
+        </View>
+        <View>
+          <Pressable style={styles.submit} onPress={validation}>
+            <Text style={styles.buttonLabel}>Save</Text>
+          </Pressable>
+        </View>
       </View>
       <Image source={{ uri: selectedImage }} style={styles.image} />
-
-      <View style={styles.box}>
-        <Pressable style={styles.button} onPress={this.onSave}>
-          <Text style={styles.buttonLabel}>Save</Text>
-        </Pressable>
-      </View>
     </ScrollView >
   )
 }
@@ -218,10 +314,6 @@ const styles = StyleSheet.create({
     margin: 5,
     pattingTop: 10,
     paddingBottom: 10,
-  },
-  link: {
-    color: '#0000FF',
-    textDecorationLine: 'underline',
   },
   swap: {
     backgroundColor: '#b4b4c0',
@@ -260,11 +352,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#131116',
     width: 90,
     borderRadius: 50,
-    marginTop: 10,
+    marginTop: 50,
     marginBottom: 10,
   },
+  submit: {
+    backgroundColor: '#131116',
+    width: 90,
+    borderRadius: 50,
+    marginBottom: 10,
+    marginLeft: 150,
+  },
   image: {
-    width: 300,
-    height: 300,
+    width: 200,
+    height: 200,
+    marginLeft: 10,
+    marginBottom: 40,
+  },
+  cardBox: {
+    flexDirection: 'row',
+    margin: 2,
+  },
+  imageSmall: {
+    width: 150,
+    height: 150,
+    marginRight: 10,
+  },
+  cardTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    paddingLeft: 15,
+  },
+  cardText: {
+    fontSize: 18,
+  },
+  link: {
+    color: '#0000FF',
+    textDecorationLine: 'underline',
+    fontSize: 18,
   },
 });
